800c7fb12c5e8fabfa9a46933a04222a4eaa28f8
cocobabb <cocobab05108@gmail.com>
2024-12-12 18:59:22 +0900

    Dog.java

---
+++ code/java/practice/src/main/java/org/example/Dog.java
@@ -0,0 +1,21 @@
+package org.example;
+
+public class Dog {
+    String breed;
+    String name;
+
+    public Dog(String breed, String name) {
+        this.breed = breed;
+        this.name = name;
+    }
+
+    public void SeatDown(){
+        System.out.println("breed = " + breed);
+        System.out.println(name+" Seat down");
+    }
+    public void giveHand(){
+        System.out.println("breed = " + breed);
+        System.out.println(name+" Give hand");
+    }
+
+}
