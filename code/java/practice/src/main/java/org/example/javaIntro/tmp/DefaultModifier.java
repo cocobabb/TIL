4404d2d895708c216e38f33126cfec18959801e1
cocobabb <cocobab05108@gmail.com>
2024-12-16 00:30:24 +0900

    public variable and default variable

---
+++ code/java/practice/src/main/java/org/example/javaIntro/tmp/DefaultModifier.java
@@ -0,0 +1,8 @@
+package org.example.javaIntro.tmp;
+
+public class DefaultModifier {
+    public String publicString = "public"; //public 변수
+    String defaultString = "default"; //default 변수
+    
+    
+}
